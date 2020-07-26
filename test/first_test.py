537a6a6feb580478b582a1ceb9fdec72308391c6
Ruiyang Zhang <ruiyang.zhang@fundingsocieties.com>
2020-07-27 01:23:50 +0800

    day-1

---
+++ test/first_test.py
@@ -0,0 +1,8 @@
+from airflow.models import DagBag
+
+def test_dag_loading():
+    dagbag = DagBag()
+    dag = dagbag.get_dag(dag_id='my_dag')
+    assert dagbag.import_errors == {}
+    assert dag is not None
+    assert len(dag.tasks) == 1
